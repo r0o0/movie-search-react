dbbd6d35b7ddf5b37b9376e86a7953e1e8b44300
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-26 18:16:47 +0900

    add language state & change language method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.sass';
+import { el } from './Assets/Helpers';
 import SelectLang from './Components/Lang';
 import SearchBar from './Components/Search';
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      lang : el('html').getAttribute('lang')
+    };
+    this.changeLang = this.changeLang.bind(this);
+    console.log('app', this.changeLang, this.state);
+  }
+  changeLang(newLang) {
+    this.setState({
+      lang: newLang
+    });
+  }
   componentDidMount() {
     const key = process.env.REACT_APP_TMDb_API_KEY;
 
@@ -14,16 +27,15 @@ class App extends Component {
     .then(res => res.json())
     .then(data => console.log(data));
   }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Movie Search React Appt</h1>
-          <SelectLang />
+          <SelectLang onChange={this.changeLang} />
         </header>
-        <SearchBar />
+        <SearchBar lang={this.state.lang} />
       </div>
     );
   }
