d1a424a6dc4085ee443076ca7f005ecaaed730d0
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-26 18:18:32 +0900

    fix handleChange method in Lang.js

--- src/Components/Lang.js
+++ src/Components/Lang.js
@@ -4,23 +4,22 @@ import { el } from '../Assets/Helpers';
 class SelectLang extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'EN'};
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
     const body = el('html');
-    let body_lang = e.target.value;
-    
-    this.setState({value: body_lang});
-    body.setAttribute('lang', body_lang);
+    const lang = e.target.value;
+
+    body.setAttribute('lang', lang);
+    this.props.onChange(lang);
   }
 
   render() {
     return (
       <label htmlFor="lang-select">Choose a language:
-        <select id="lang-select" value={this.state.value} onChange={this.handleChange}>
+        <select id="lang-select" onChange={this.handleChange}>
           <option value="en">English</option>
           <option value="ko-KR">한국어</option>
           <option value="fr">Français</option>
