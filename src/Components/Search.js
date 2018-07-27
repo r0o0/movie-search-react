4bcbe7df80c6221cf87f7bb497ffe4825028f2bf
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-27 13:31:44 +0900

    add tmdb api fetch

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import SearchForm from './SearchForm';
 
 class MovieSearch extends Component {
+  componentDidMount() {
+    const key = process.env.REACT_APP_TMDb_API_KEY;
+
+    // get image url from TMDb API
+    fetch(`//api.themoviedb.org/3/configuration?api_key=${key}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
   render() {
   return (
     <main>
