b6363ab2254210b77763f11035971c8adcb5d757
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-27 13:20:06 +0900

    remove form jsx && add SearchForm component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import IconSearch from '../Assets/Icons/iconSearch';
+import SearchForm from './SearchForm';
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lang: 'en',
-      placeholder: 'Search a movie',
-      name: 'Search Bar',
-      button: 'Search'
-    };
-  }
-
+class MovieSearch extends Component {
   render() {
-    return (
-      <form id="form-search"> 
-        <input type="text" id="mvs" value="" autocomplete="off" placeholder={this.state.placeholder} aria-label={this.state.name} />
-          <button type="button" id="send">
-            <span class="a11y-hidden">{this.state.button}</span>
-            <IconSearch />
-          </button>
-      </form>
-    );
+  return (
+    <main>
+      <SearchForm lang={this.props.lang} />
+    </main>
+  );
   }
 }
 
-export default SearchBar;
+export default MovieSearch;
