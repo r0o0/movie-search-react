b8d2fdffe5e90e282f4a7b7bc24676eba9e83626
r0o0 <rayeoh7@gmail.com>
2018-07-29 19:27:40 +0900

    add browser default style reset css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Assets/reset.css';
 import './index.sass';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
