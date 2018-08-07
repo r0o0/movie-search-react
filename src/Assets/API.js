889063e9f31ad4b3ee5f57833fa892d5aa26eeb2
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-08-07 11:20:09 +0900

    fix tmdb api image url to be secure url -> https

--- src/Assets/API.js
+++ src/Assets/API.js
@@ -6,7 +6,7 @@ fetch(`//api.themoviedb.org/3/configuration?api_key=${key}`)
   .then(res => res.json())
   .then(data => {
     console.log(data.images.poster_sizes);
-    tmdb.imgURL = data.images.base_url;
+    tmdb.imgURL = data.images.secure_base_url;
     tmdb.posterSizes = data.images.poster_sizes;
     tmdb.p_retina = tmdb.posterSizes[5];
     tmdb.p_default = tmdb.posterSizes[4];
